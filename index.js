14dfc97178f00751189b40455b0220c43bd8e7ea
Zulfikar Muhamad <zulfikarmuhamad432@gmail.com>
2023-05-08 12:09:32 +0700

    commit pertama

---
+++ index.js
@@ -0,0 +1,8 @@
+const tampilkanBiodata = require('./moduleBiodata');
+
+const nama = 'Zulfikar Muhamad',
+  tempatLahir = 'Bogor',
+  tanggalLahir = '18 Oktober 2000',
+  alamat = 'Kp.kebon kelapa rt07/04, Ds.cimanadala, Kec.sukaraja, Kab.Bogor';
+
+tampilkanBiodata(nama, tempatLahir, tanggalLahir, alamat);
---
+++ moduleBiodata.js
@@ -0,0 +1,12 @@
+function tampilkanBiodata(nama, tempatLahir, tanggalLahir, alamat) {
+  console.log(
+    `
+    Nama : ${nama}
+    Tempat Lahir : ${tempatLahir}
+    Tanggal Lahir : ${tanggalLahir}
+    Alamat : ${alamat}
+    `
+  );
+}
+
+module.exports = tampilkanBiodata;
